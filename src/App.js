8b468ebc58a2025c6f9327e3dca77c8499842ef8
kumar sundram <kumar@reckonsys.com>
2021-06-27 13:37:59 +0530

    priviate route and authenticated store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import React from "react";
-import {BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import './App.css';
+import "./App.css";
+import UrlConfig from "./config";
+import { useStoreActions, useStoreState } from "easy-peasy";
 
+const AuthChecker = () => {
+  const isAuth = useStoreState(state => state.isAuthorised);
+  const setIsAuthorised = useStoreActions(actions => actions.setIsAuthorised);
+  const [isLoading, setIsLoading] = useState(true)
 
-const  App = () => {
+  useEffect(() => {
+    handleAuth()
+      .then((res) => {
+        if (res.status === "success") {
+          setIsAuthorised(true)
+        } else {
+          setIsAuthorised(false)
+        }
+        setIsLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <></>
+  }
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={"/login"} component={Login} />
-        <Route path={"/home"} component={Home} />
-        <Redirect from={"/"} to={"/login"} />
+        {!isAuth && <Route path={"/login"} component={Login} />}
+        {isAuth && <Route path={"/home"} component={Home} exact />}
+        {!isAuth && <Redirect to="/login" />}
+        {isAuth && <Redirect to="/home" />}
       </Switch>
     </BrowserRouter>
   );
 }
+
+const handleAuth = () => {
+  const protect = `${UrlConfig.BASE_URL}/user/protect`
+  const options = {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  }
+  return fetch(protect, options)
+    .then(response => response.json())
+    .then((res) => res)
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={"/"} component={AuthChecker} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,78 @@
 import React, { useState } from "react";
+import UrlConfig from "../config";
+import { useStoreActions } from "easy-peasy";
 
-const Login = () => {
+const Login = ({ history, setIsAuth }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const setIsAuthorised = useStoreActions(actions => actions.setIsAuthorised);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
-    return (
-        <div className="container">
-            <div className="login-card">
-                <p className="title">
-                   Admin Login
-                </p>
-                <div className="input-container">
-                    <p className="input-label">Email</p>
-                    <input className="input" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-                </div>
-                <div className="input-container">
-                    <p className="input-label">Password</p>
-                    <input className="input" type="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
-                </div>
-                <button className="btn" disabled={!email || !password} onClick={() => console.log("click")}>Log In</button>
-            </div>
+  const handleLogin = () => {
+    const login = `${UrlConfig.BASE_URL}/user/login`
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+      headers: {
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    }
+    fetch(login, options)
+      .then(response => response.json())
+      .then((data) => {
+        if (data.error) {
+          setError(true)
+          alert(data.error)
+          return
+        }
+        setIsAuthorised(true)
+        history.push('/home')
+      }).catch((error) => {
+        setError(true)
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="login-card">
+        <p className="title">Admin Login</p>
+        <div className="input-container">
+          <p className="input-label">Email</p>
+          <input
+            className="input"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
-    );
-}
+        <div className="input-container">
+          <p className="input-label">Password</p>
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        {error && <p className="error">{"Error!"}</p>}
+        <button
+          className="btn"
+          disabled={!email || !password}
+          onClick={handleLogin}
+        >
+          Log In
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
